9295bde43f0550fc3facf907d308633d0cf94654
Alissin82 <aliansaipoor82@gmail.com>
2023-01-06 20:33:24 +0330

    developing admin dashboard front

--- asset/includes/head.php
+++ asset/includes/head.php
@@ -1,9 +1,23 @@
-<!-- Font Awesome -->
+<?php 
+$project_title;
+function title($txt=""){
+    $project_title = "مهاجرآنلاین";
+    echo $project_title . $txt;}
+?>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <script src="https://kit.fontawesome.com/06ea7b8ed9.js" crossorigin="anonymous"></script>
-      
-<!-- Theme style -->
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css"
     integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
 </script>
+
+<link rel="shortcut icon" type="image/jpg" href="asset/img/school_logo.ico"/>
+<link rel="shortcut icon" type="image/jpg" href="../asset/img/school_logo.ico"/>
+<link rel="stylesheet" href="asset/css/style.css" type="text/css">
+
+</style>
--- login.php
+++ login.php
@@ -2,22 +2,11 @@
 <html lang="fa" dir="rtl">
 
    <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title> <?php include_once("asset/php/title.php"); title(); ?> </title>
-      
-      <!-- Font Awesome -->
-      <script src="https://kit.fontawesome.com/06ea7b8ed9.js" crossorigin="anonymous"></script>
-      
-      <!-- Theme style -->
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css"
-         integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous">
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-      </script>
+      <?php include_once("asset/includes/head.php"); ?>
+      <title><?php title(); ?></title>
    </head>
 
-   <body style="height: 100vh;" class="d-flex justify-content-center align-items-center">
+   <body class="d-flex justify-content-center align-items-center">
       <div class="card col-10 col-md-4">
          <div class="card-header text-center">
             <img src="asset\img\school_logo.png" alt="school_logo" width="200px">
